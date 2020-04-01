2e6abddac9f14b51e70f25bd071b845552a8e30c
Philipp Cuntz <philipp@cuntz.org>
2020-04-01 22:50:03 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'binance adapter settings': {
+        'en': 'Adapter settings for binance',
+        'de': 'Adaptereinstellungen für binance',
+        'ru': 'Настройки адаптера для binance',
+        'pt': 'Configurações do adaptador para binance',
+        'nl': 'Adapterinstellingen voor binance',
+        'fr': "Paramètres d'adaptateur pour binance",
+        'it': "Impostazioni dell'adattatore per binance",
+        'es': 'Ajustes del adaptador para binance',
+        'pl': 'Ustawienia adaptera dla binance',
+        'zh-cn': 'binance的适配器设置'
+    },
+    'option1': {
+        'en': 'option1',
+        'de': 'Option 1',
+        'ru': 'Опция 1',
+        'pt': 'Opção 1',
+        'nl': 'Optie 1',
+        'fr': 'Option 1',
+        'it': 'opzione 1',
+        'es': 'Opción 1',
+        'pl': 'opcja 1',
+        'zh-cn': '选项1'
+    },
+    'option2': {
+        'en': 'option2',
+        'de': 'Option 2',
+        'ru': 'option2',
+        'pt': 'opção 2',
+        'nl': 'Optie 2',
+        'fr': 'Option 2',
+        'it': 'opzione 2',
+        'es': 'opcion 2',
+        'pl': 'Opcja 2',
+        'zh-cn': '选项2'
+    }
+};

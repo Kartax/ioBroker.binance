3c4cf9df1a53048ffc76338e98073f7c1e17a273
Philipp Cuntz <philipp@cuntz.org>
2020-04-02 20:06:37 +0200

    WIP

--- admin/words.js
+++ admin/words.js
@@ -1,29 +1,9 @@
 /*global systemDictionary:true */
 'use strict';
 
-systemDictionary = {
-    'binance adapter settings': {
-        'en': 'Adapter settings for binance',
-        'de': 'Adaptereinstellungen für binance',
-        'ru': 'Настройки адаптера для binance',
-        'pt': 'Configurações do adaptador para binance',
-        'nl': 'Adapterinstellingen voor binance',
-        'fr': "Paramètres d'adaptateur pour binance",
-        'it': "Impostazioni dell'adattatore per binance",
-        'es': 'Ajustes del adaptador para binance',
-        'pl': 'Ustawienia adaptera dla binance',
-        'zh-cn': 'binance的适配器设置'
-    },
-    'interval': {
-        'en': 'Interval',
-        'de': 'Interval'
-    },
-    'second': {
-        'en': 'Second',
-        'de': 'Sekunde'
-    },
-    'seconds': {
-        'en': 'Seconds',
-        'de': 'Sekunden'
+const systemDictionary = {
+    'update-interval': {
+        'en': 'Update Interval',
+        'de': 'Update Interval'
     }
 };

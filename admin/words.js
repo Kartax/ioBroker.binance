57350f91086f6af51ba7d4d43331523554462517
Philipp Cuntz <philipp@cuntz.org>
2020-04-07 15:43:35 +0200

    fixed translations in words.js

--- admin/words.js
+++ admin/words.js
@@ -15,59 +15,51 @@ systemDictionary = {
         "zh-cn": "Binance API适配器"
     },
     'adapter-description': {
-        "The adapter downloads prices of currencies in the configured update interval. If you configure an API Key and the corresponding secret, it will pull account balances as well. You may create an API Key on binance.com - i suggest to restrict it to \"read-only": {
-            "en": "The adapter downloads prices of currencies in the configured update interval. If you configure an API Key and the corresponding secret, it will pull account balances as well. You may create an API Key on binance.com - i suggest to restrict it to \"read-only",
-            "de": "Der Adapter lädt Währungspreise im konfigurierten Aktualisierungsintervall herunter. Wenn Sie einen API-Schlüssel und das entsprechende Geheimnis konfigurieren, werden auch Kontensalden abgerufen. Sie können einen API-Schlüssel auf binance.com erstellen. Ich empfehle, ihn auf \"schreibgeschützt\" zu beschränken",
-            "ru": "Адаптер загружает цены валют в заданном интервале обновления. Если вы настроите ключ API и соответствующий секретный ключ, он также потянет остатки на счетах. Вы можете создать ключ API на binance.com - я предлагаю ограничить его только для чтения",
-            "pt": "O adaptador baixa os preços das moedas no intervalo de atualização configurado. Se você configurar uma chave de API e o segredo correspondente, também obterá saldos da conta. Você pode criar uma chave de API no binance.com - sugiro restringi-la a \"somente leitura\"",
-            "nl": "De adapter downloadt prijzen van valuta's in het geconfigureerde update-interval. Als u een API-sleutel en het bijbehorende geheim configureert, worden ook de rekeningsaldi opgehaald. U kunt op binance.com een API-sleutel maken. Ik raad u aan deze te beperken tot 'alleen-lezen'",
-            "fr": "L'adaptateur télécharge les prix des devises dans l'intervalle de mise à jour configuré. Si vous configurez une clé API et le secret correspondant, elle tirera également les soldes des comptes. Vous pouvez créer une clé API sur binance.com - je suggère de la limiter à \"lecture seule\"",
-            "it": "L'adattatore scarica i prezzi delle valute nell'intervallo di aggiornamento configurato. Se si configura una chiave API e il segreto corrispondente, verranno estratti anche i saldi degli account. È possibile creare una chiave API su binance.com - suggerisco di limitarla a \"sola lettura",
-            "es": "El adaptador descarga los precios de las monedas en el intervalo de actualización configurado. Si configura una clave API y el secreto correspondiente, también extraerá los saldos de las cuentas. Puede crear una clave API en binance.com; sugiero restringirla a \"solo lectura\"",
-            "pl": "Adapter pobiera ceny walut w skonfigurowanym interwale aktualizacji. Jeśli skonfigurujesz klucz API i odpowiedni klucz tajny, spowoduje to również wyciągnięcie sald kont. Możesz utworzyć Klucz API na binance.com - sugeruję ograniczenie go do „tylko do odczytu",
-            "zh-cn": "适配器以配置的更新间隔下载货币价格。如果您配置了API密钥和相应的机密，它也会提取帐户余额。您可以在binance.com上创建一个API密钥-我建议将其限制为“只读”"
-        }
+        "en": "The adapter downloads prices of currencies in the configured update interval. If you configure an API Key and the corresponding secret, it will pull account balances as well. You may create an API Key on binance.com - i suggest to restrict it to \"read-only",
+        "de": "Der Adapter lädt Währungspreise im konfigurierten Aktualisierungsintervall herunter. Wenn Sie einen API-Schlüssel und das entsprechende Geheimnis konfigurieren, werden auch Kontensalden abgerufen. Sie können einen API-Schlüssel auf binance.com erstellen. Ich empfehle, ihn auf \"schreibgeschützt\" zu beschränken",
+        "ru": "Адаптер загружает цены валют в заданном интервале обновления. Если вы настроите ключ API и соответствующий секретный ключ, он также потянет остатки на счетах. Вы можете создать ключ API на binance.com - я предлагаю ограничить его только для чтения",
+        "pt": "O adaptador baixa os preços das moedas no intervalo de atualização configurado. Se você configurar uma chave de API e o segredo correspondente, também obterá saldos da conta. Você pode criar uma chave de API no binance.com - sugiro restringi-la a \"somente leitura\"",
+        "nl": "De adapter downloadt prijzen van valuta's in het geconfigureerde update-interval. Als u een API-sleutel en het bijbehorende geheim configureert, worden ook de rekeningsaldi opgehaald. U kunt op binance.com een API-sleutel maken. Ik raad u aan deze te beperken tot 'alleen-lezen'",
+        "fr": "L'adaptateur télécharge les prix des devises dans l'intervalle de mise à jour configuré. Si vous configurez une clé API et le secret correspondant, elle tirera également les soldes des comptes. Vous pouvez créer une clé API sur binance.com - je suggère de la limiter à \"lecture seule\"",
+        "it": "L'adattatore scarica i prezzi delle valute nell'intervallo di aggiornamento configurato. Se si configura una chiave API e il segreto corrispondente, verranno estratti anche i saldi degli account. È possibile creare una chiave API su binance.com - suggerisco di limitarla a \"sola lettura",
+        "es": "El adaptador descarga los precios de las monedas en el intervalo de actualización configurado. Si configura una clave API y el secreto correspondiente, también extraerá los saldos de las cuentas. Puede crear una clave API en binance.com; sugiero restringirla a \"solo lectura\"",
+        "pl": "Adapter pobiera ceny walut w skonfigurowanym interwale aktualizacji. Jeśli skonfigurujesz klucz API i odpowiedni klucz tajny, spowoduje to również wyciągnięcie sald kont. Możesz utworzyć Klucz API na binance.com - sugeruję ograniczenie go do „tylko do odczytu",
+        "zh-cn": "适配器以配置的更新间隔下载货币价格。如果您配置了API密钥和相应的机密，它也会提取帐户余额。您可以在binance.com上创建一个API密钥-我建议将其限制为“只读”"
     },
     'update-interval': {
-        "Update Interval": {
-            "en": "Update Interval",
-            "de": "Updateintervall",
-            "ru": "Интервал обновления",
-            "pt": "Intervalo de atualização",
-            "nl": "Bijwerkingsonderbreking",
-            "fr": "Intervalle de mise à jour",
-            "it": "Intervallo di aggiornamento",
-            "es": "Intervalo de actualización",
-            "pl": "Interwał aktualizacji",
-            "zh-cn": "更新间隔"
-        }
+        "en": "Update Interval",
+        "de": "Updateintervall",
+        "ru": "Интервал обновления",
+        "pt": "Intervalo de atualização",
+        "nl": "Bijwerkingsonderbreking",
+        "fr": "Intervalle de mise à jour",
+        "it": "Intervallo di aggiornamento",
+        "es": "Intervalo de actualización",
+        "pl": "Interwał aktualizacji",
+        "zh-cn": "更新间隔"
     },
     'apiKey': {
-        "API Key": {
-            "en": "API Key",
-            "de": "API Schlüssel",
-            "ru": "Ключ API",
-            "pt": "Chave API",
-            "nl": "API sleutel",
-            "fr": "clé API",
-            "it": "Chiave API",
-            "es": "Clave API",
-            "pl": "Klucz API",
-            "zh-cn": "API密钥"
-        }
+        "en": "API Key",
+        "de": "API Schlüssel",
+        "ru": "Ключ API",
+        "pt": "Chave API",
+        "nl": "API sleutel",
+        "fr": "clé API",
+        "it": "Chiave API",
+        "es": "Clave API",
+        "pl": "Klucz API",
+        "zh-cn": "API密钥"
     },
     'apiKeySecret': {
-        "API Key secret": {
-            "en": "API Key secret",
-            "de": "API Schlüssel Geheimnis",
-            "ru": "Секретный ключ API",
-            "pt": "Segredo da chave da API",
-            "nl": "API-sleutelgeheim",
-            "fr": "Secret de clé API",
-            "it": "Segreto chiave API",
-            "es": "Secreto clave de API",
-            "pl": "Klucz tajny API",
-            "zh-cn": "API密钥机密"
-        }
+        "en": "API Key secret",
+        "de": "API Schlüssel Geheimnis",
+        "ru": "Секретный ключ API",
+        "pt": "Segredo da chave da API",
+        "nl": "API-sleutelgeheim",
+        "fr": "Secret de clé API",
+        "it": "Segreto chiave API",
+        "es": "Secreto clave de API",
+        "pl": "Klucz tajny API",
+        "zh-cn": "API密钥机密"
     }
 };

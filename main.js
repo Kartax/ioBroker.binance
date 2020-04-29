64ad01c7d0785303131439ab28371e8cb9a2604c
Philipp Cuntz <philipp@cuntz.org>
2020-04-29 21:25:21 +0200

    WIP 24hr data

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ class Binance extends utils.Adapter {
      * The main update method
      */
     main() {
-        if (this.config.allPrices) this.requestPrices();
+        if (this.config.allPrices === 'yes') this.requestPrices();
         if (this.config.symbols) this.request24hr();
         if (this.config.apiKey && this.config.apiKeySecret) this.requestAccount();
     }
